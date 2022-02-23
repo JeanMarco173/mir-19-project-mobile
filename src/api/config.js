7f6c96f8e62983f4b7c5201d7fd4e17cea6184bc
JeanMarko173 <jeanmarko.13.7@gmail.com>
2022-02-23 09:18:44 -0500

    fix gitignore

--- src/api/config.js
+++
@@ -1,4 +0,0 @@
-export const googleToken = "AIzaSyAbp1pZlN1yas1HUWw3wV_df_TjuoHknRM";
-
-export const googleMapsAPI = `https://maps.googleapis.com/maps/api/place/`;
-export const googleMapsRouteAPI = `https://maps.googleapis.com/maps/api/directions/`;
